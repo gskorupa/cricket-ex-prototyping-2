38e25b0374e68004633c92f1d03f4b469b025fb4
greg <g.skorupa@gmail.com>
2018-06-29 23:42:49 +0200

    fixed paths

--- run-tasks.sh
+++ run-tasks.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java --illegal-access=deny --add-modules java.xml.bind --add-modules java.activation --add-opens java.base/java.util=java.xml.bind -cp /home/greg/test/build/classes:/home/greg/test/cricket-1.2.46.jar org.cricketmsf.Runner -c src/tasks.json -s Microsite -r
+java --illegal-access=deny --add-modules java.xml.bind --add-modules java.activation --add-opens java.base/java.util=java.xml.bind -cp build/classes:cricket-1.2.46.jar org.cricketmsf.Runner -c src/tasks.json -s Microsite -r
